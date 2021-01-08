31ef198d77883efb9f97eb9ec16e77734cfa86c8
Luiz Marcelo Freire <luiz.marcelo-developer@outlook.com>
2021-01-08 11:12:24 -0400

    Add project files.

---
+++ src/DevCurso.Site/Controllers/TesteCrudController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCurso.Site.Data;
+using DevCurso.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCurso.Site.Controllers
+{
+    public class TesteCrudController : Controller
+    {
+        private readonly MeuDbContext _context;
+        public TesteCrudController(MeuDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno
+            {
+                Nome = "Luiz Marcelo",
+                DataNascimento = DateTime.Now,
+                Email = "luiz@luiz.com"
+            };
+
+            _context.Alunos.Add(aluno);
+            _context.SaveChanges();
+
+            BuscarAluno(aluno);
+
+            UpdateAluno(aluno);
+
+            RemoverAluno(aluno);
+
+            return View();
+        }
+
+        private void RemoverAluno(Aluno aluno)
+        {
+            _context.Alunos.Remove(aluno);
+            _context.SaveChanges();
+        }
+
+        private void UpdateAluno(Aluno aluno)
+        {
+            aluno.Nome = "João";
+            _context.Alunos.Update(aluno);
+            _context.SaveChanges();
+        }
+
+        private void BuscarAluno(Aluno aluno)
+        {
+            var aluno2 = _context.Alunos.Find(aluno.Id);
+            var aluno3 = _context.Alunos.FirstOrDefault(x => x.Email.Equals("luiz@luiz.com"));
+            var aluno4 = _context.Alunos.Where(x => x.Nome == "Luiz Marcelo").ToList(); 
+        }
+    }
+}
---
+++ src/DevCurso.Site/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCurso.Site.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevCurso.Site
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration _configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MeuDbContext>(options => 
+            options.UseSqlServer(_configuration.GetConnectionString("MeuDbContext")));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            } 
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
